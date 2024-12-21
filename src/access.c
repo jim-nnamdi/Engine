0d3023189ace938575a3cdee6ae5d63dd0661856
Jim <jimsamuel50@gmail.com>
2024-12-21 17:44:36 +0100

    increase entry sizes and disable visibility for password entry

--- src/access.c
+++ src/access.c
@@ -35,10 +35,13 @@ void add_new_trader(GtkApplication* app, gpointer user_data){
     GtkWidget* email_entry = gtk_entry_new();
     gtk_grid_attach(GTK_GRID (ntrader_grid), email_entry, 3,3,1,1);
     gtk_entry_set_placeholder_text(GTK_ENTRY (email_entry), "email address");
+    gtk_widget_set_size_request(email_entry, 200, 40);
 
     GtkWidget* passw_entry = gtk_entry_new();
     gtk_grid_attach(GTK_GRID (ntrader_grid), passw_entry, 3,4,1,1);
     gtk_entry_set_placeholder_text(GTK_ENTRY (passw_entry), "secure password");
+    gtk_widget_set_size_request(passw_entry, 200, 40);
+    gtk_entry_set_visibility(GTK_ENTRY(passw_entry), false);
     gtk_widget_set_margin_top(passw_entry, 5);
 
     User_Data_Model *user_model = g_new(User_Data_Model, 1);
