0a7fcee274a84c917e2dc4a2e6f2c884cd4df909
Jim <jimsamuel50@gmail.com>
2024-12-22 07:16:01 +0100

    sodium for data encryption . security basis failed at this point

--- makefile
+++ makefile
@@ -1,11 +1,11 @@
 # Compiler and flags
 CC = gcc
-CFLAGS = $(shell pkg-config --cflags gtk4)
-LIBS = $(shell pkg-config --libs gtk4) -lsqlite3
+CFLAGS = $(shell pkg-config --cflags gtk4 libsodium)
+LIBS = $(shell pkg-config --libs gtk4 libsodium) -lsqlite3 
 
 # Target executable and source file
 TARGET = main
-SRC = ./src/main.c ./src/access.c ./src/globals.c ./database/database.c
+SRC = ./src/main.c ./src/access.c ./src/globals.c ./database/database.c ./security/safe.c
 
 # Build target
 all: $(TARGET)
---
+++ security/safe.c
@@ -0,0 +1,14 @@
+#include "safe.h"
+#include <string.h>
+
+int encrypt_password(const char* password, unsigned char* ciphertext, unsigned char* nonce, unsigned char* key) {
+    randombytes_buf(key,crypto_secretbox_KEYBYTES);
+    randombytes_buf(nonce, crypto_secretbox_NONCEBYTES);
+    int encrypt = crypto_secretbox_easy(ciphertext, (unsigned char*) password, strlen(password) + 1, nonce, key);
+    return encrypt;
+}
+
+int decrypt_password(const unsigned char* ciphertext, unsigned char* decrypted_password, size_t ciphertext_len, unsigned char* nonce, unsigned char* key){
+    if (crypto_secretbox_open_easy(decrypted_password,ciphertext,ciphertext_len,nonce, key) != 0) return 1;
+    return 0;
+}
